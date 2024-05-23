84c752152e8ebca9d360e7af9ca395ab361478ca
Grimm <fvckgrimm@protonmail.ch>
2024-05-23 13:59:45 -0700

    Add more security services

--- modules/nixos/security/default.nix
+++ modules/nixos/security/default.nix
@@ -5,9 +5,12 @@
   # stole these from https://github.com/XNM1/linux-nixos-hyprland-config-dotfiles/ 
   imports = [
     ./firewall.nix
-    ./mac-randomize.nix
+    #./mac-randomize.nix
     ./info-fetchers.nix
     ./virtualisation.nix
+    #./usb.nix
+    #./security-services.nix
+    #./clamav-scanner.nix
   ];
 
 }
