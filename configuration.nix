2cc1b35b85281c170dbe9aa435bff5e8fcb5f73a
Grimm <fvckgrimm@protonmail.ch>
2024-06-15 09:51:45 -0700

    Rename options

--- configuration.nix
+++ configuration.nix
@@ -88,8 +88,10 @@
 
   # Configure keymap in X11
   services.xserver = {
-    layout = "us";
-    xkbVariant = "";
+    xkb = {
+      layout = "us";
+      variant = "";
+    };
   };
 
   # Enable CUPS to print documents.
