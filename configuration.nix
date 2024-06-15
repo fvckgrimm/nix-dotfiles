7ec894e7b8353d47b156657de6a83e07370181b1
Grimm <fvckgrimm@protonmail.ch>
2024-06-15 13:37:27 -0700

    Driver for wifi adapter???

--- configuration.nix
+++ configuration.nix
@@ -26,6 +26,7 @@
   #powerManagement.powerUpCommands = "sudo rmmod atkbd; sudo modprobe atkbd reset=1";
   powerManagement.resumeCommands = "sudo ${pkgs.kmod}/bin/rmmod atkbd; sudo ${pkgs.kmod}/bin/modprobe atkbd reset=1";
 
+  boot.extraModulePackages = [ config.boot.kernelPackages.rtl8821ce ];
 
   services.logind.extraConfig = ''
     HandlePowerKey=suspend-then-hibernate
