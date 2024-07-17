5e00e9e0f3705243a6c09054da37cdd32d18c917
Grimm <fvckgrimm@protonmail.ch>
2024-07-17 14:04:32 -0700

    update: zellij attach

--- modules/home-manager/shell/sh.nix
+++ modules/home-manager/shell/sh.nix
@@ -30,6 +30,31 @@
       highlighters = ["main" "brackets" "pattern" "cursor" "regexp" "root" "line"];
     };
 
+    initExtra = ''
+      za() {
+        if [ -z $1 ]; then
+          SELECTION=$(
+            zellij list-sessions \
+              --no-formatting \
+              | awk '{ print $1 }' \
+              | gum choose \
+                --cursor="▌" \
+                --header="Select a session to attach to" \
+                --selected="ide" \
+                --ordered --select-if-one --cursor.foreground="139"
+          )
+        else
+          SELECTION=$1
+        fi
+
+        if [ -z $SELECTION ]; then
+          return 0
+        fi
+
+        zellij attach "$(echo -e $SELECTION)"
+      }
+    '';
+
     shellAliases = {
       # NIX related
       switch-build = "sudo nixos-rebuild switch --flake .#desolate";
@@ -41,7 +66,7 @@
       search = "fzf --preview 'bat --color=always --style=numbers --line-range=:500 {}' | xargs nvim ";
       ls = "eza -L=1 -lhTag";
       zc = "zellij -s";
-      za = "zellij a";
+      #za = "zafunc";
       zl = "zellij ls";
       tree = "eza --tree --icons --tree";
       cd = "z";
@@ -52,10 +77,8 @@
       grep= "rg";
 
       ytmp3 = "yt-dlp --ignore-errors -x --audio-format mp3 -f bestaudio --audio-quality 0 --embed-metadata --embed-thumbnail --output '%(title)s.%(ext)s'";
+    }; 
 
-    
-
-    };
     oh-my-zsh = {
       enable = true;
       plugins = [ "git" "urltools" "bgnotify" ];
